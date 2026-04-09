a0c5f4724867cca3913e90bdeced90f598b36fe3
sravanigaju <sravanigajula99@gmail.com>
2026-04-09 11:58:13 +0530

    modified mme

--- modules/aws/sagemaker/endpoint/outputs.tf
+++ modules/aws/sagemaker/endpoint/outputs.tf
@@ -2,8 +2,13 @@ output "endpoint_name" {
   description = "SageMaker endpoint name"
   value       = aws_sagemaker_endpoint.this.name
 }
-
+P
 output "endpoint_arn" {
   description = "SageMaker endpoint ARN"
   value       = aws_sagemaker_endpoint.this.arn
+}
+
+output "model_name" {
+  description = "Name of the SageMaker model"
+  value       = aws_sagemaker_model.this.name
 }

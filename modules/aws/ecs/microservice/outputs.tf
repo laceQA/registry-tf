508503dcfc121e4e443acf4e77a3fc851afd6430
rajiqxf2 <raji@qxf2.com>
2026-04-15 13:49:35 +0530

    Adding aws-ecs-microservice modules wiring in private registry

---
+++ modules/aws/ecs/microservice/outputs.tf
@@ -0,0 +1,105 @@
+output "cluster_arn" {
+  value = module.cluster.arn
+}
+output "cluster_id" {
+  value = module.cluster.id
+}
+output "cluster_name" {
+  value = module.cluster.name
+}
+output "listener_arn" {
+  value = module.listener.arn
+}
+output "listener_id" {
+  value = module.listener.id
+}
+output "load_balancer_arn" {
+  value = module.load_balancer.arn
+}
+output "load_balancer_dns_name" {
+  value = module.load_balancer.dns_name
+}
+output "load_balancer_id" {
+  value = module.load_balancer.id
+}
+output "load_balancer_zone_id" {
+  value = module.load_balancer.zone_id
+}
+output "log_group_log_group_arn" {
+  value = module.log_group.log_group_arn
+}
+output "log_group_log_group_name" {
+  value = module.log_group.log_group_name
+}
+output "repository_registry_id" {
+  value = module.repository.registry_id
+}
+output "repository_repository_arn" {
+  value = module.repository.repository_arn
+}
+output "repository_repository_name" {
+  value = module.repository.repository_name
+}
+output "repository_repository_url" {
+  value = module.repository.repository_url
+}
+output "role_role_arn" {
+  value = module.role.role_arn
+}
+output "role_role_name" {
+  value = module.role.role_name
+}
+output "security_group_security_group_arn" {
+  value = module.security_group.security_group_arn
+}
+output "security_group_security_group_id" {
+  value = module.security_group.security_group_id
+}
+output "security_group_security_group_name" {
+  value = module.security_group.security_group_name
+}
+output "service_cluster" {
+  value = module.service.cluster
+}
+output "service_id" {
+  value = module.service.id
+}
+output "service_name" {
+  value = module.service.name
+}
+output "target_group_arn" {
+  value = module.target_group.arn
+}
+output "target_group_id" {
+  value = module.target_group.id
+}
+output "target_group_name" {
+  value = module.target_group.name
+}
+output "task_definition_arn" {
+  value = module.task_definition.arn
+}
+output "task_definition_family" {
+  value = module.task_definition.family
+}
+output "task_definition_revision" {
+  value = module.task_definition.revision
+}
+output "vpc_cidr_block" {
+  value = module.vpc.cidr_block
+}
+output "vpc_id" {
+  value = module.vpc.id
+}
+output "vpc_internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
+output "vpc_nat_gateway_id" {
+  value = module.vpc.nat_gateway_id
+}
+output "vpc_private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+output "vpc_public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
